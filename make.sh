ee76920ce46f663e062e2fc71e3889f2879c8672
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-02-27 19:01:34 -0600

    REWRITE START

--- make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cargo build -q
-mv ./target/debug/tapm ~/.cargo/bin/tapm
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
 fn main() {
-    let mut api = tapm::api::Api::new();
-
-    let flags: Vec<String> = std::env::args().collect();
-    api.process_flags(&flags);
-
-    let mpass: String;
-    let mut site = String::new();
-    let mut id = String::new();
-
-    if api.flag_s {
-        mpass = rpassword::prompt_password("Single: ").unwrap();
-    } else {
-        mpass = rpassword::prompt_password("Master: ").unwrap();
-        site = rpassword::prompt_password("Site: ").unwrap();
-        id = rpassword::prompt_password("ID(opt): ").unwrap();
-    }
-
-    api.main(&mpass, &site, &id, &flags);
+    println!("rewrite");
 }
