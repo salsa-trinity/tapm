1d7e78aee18ffcbc6572b322418e40293529e70e
salsa-trinity <salsa-trinity-wavy@duck.com>
2025-03-06 17:08:15 -0600

    made the basic functionality work

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,9 @@ use tapm::cli::args::Args;
 
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
-    let args = Args::parse();
-    println!("{:?}", args);
-    loop {
-        let mut seed = String::new();
-        std::io::stdin().read_line(&mut seed).unwrap();
-        println!("hash: {}", tapm::api::tapm::clean_hash(&seed));
-        println!("");
-    }
+    Args::parse();
+
+    let mut seed = String::new();
+    std::io::stdin().read_line(&mut seed).unwrap();
+    println!("password: {}", tapm::api::tapm::clean_hash(&seed));
 }
